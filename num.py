41fb2013f77be32dd55dbcb2504510e06e21eb60
beatles213 <23313963@qq.com>
2018-01-09 10:31:24 +0800

    create file of num

---
+++ num.py
@@ -0,0 +1,31 @@
+import this
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+
+#两个**表示乘方
+print(3**2)
+print(3**3)
+print(10**6)
+
+print(2+3*4)
+print((2+3)*4)
+
+#浮点数
+print(0.1+0.1)
+print(0.2+0.2)
+print(2*0.1)
+print(2*0.2)
+#结果包含的小数位数是不确定的
+print(0.1+0.2)
+print(3*0.1)
+
+#str（）函数
+age=23
+#将数值转化成字符串
+message="Happy"+str(age)+"rd Birthday!"
+print(message)
+
+#除
+print(3/2)
