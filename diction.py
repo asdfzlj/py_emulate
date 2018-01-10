20c55caba75e8f3db57aef7403465bd9ee873565
beatles213 <23313963@qq.com>
2018-01-10 11:37:44 +0800

    create file of list

--- diction.py
+++ diction.py
@@ -41,3 +41,36 @@ for k,v in student.items():
 #遍历所有的key
 for k in student.keys():
     print(k)
+
+#嵌套
+alien_0={'color':'green','points':5}
+alien_1={'color':'yellow','points':10}
+alien_2={'color':'red','points':15}
+
+aliens=[alien_0,alien_1,alien_2]
+
+for alien in aliens:
+    print(alien)
+
+
+aliens=[]
+for alien_number in range(30):
+    new_alien={'color':'green','points':5,'speed':'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+    print(alien['color'])
+print("...")
+print(len("Total number of alien:"+str(len(aliens))))
+
+#在字典里存储列表
+
+pizza={
+    'crust':'thick',
+    'toppings':['mushrooms','extra cheese']
+}
+print(pizza['crust'])
+print(pizza['toppings'])
+for topping in pizza['toppings']:
+    print(topping)
