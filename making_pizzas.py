aa380827ae882324dea3c38abc1d3b2a3e02aafe
beatles213 <23313963@qq.com>
2018-01-12 00:39:57 +0800

    modify module

--- making_pizzas.py
+++ making_pizzas.py
@@ -1,5 +1,7 @@
-import pizza
+#导入指定模块，使用as 给函数指定别名 from module import * 导入模块中所有的函数
+from module import pizza as p
 
-pizza.make_pizza(16,'pepperono')
-pizza.make_pizza(12,'aaaa','bbb','cccc')
+# pizza.make_pizza(16,'pepperono')
+# pizza.make_pizza(12,'aaaa','bbb','cccc')
 
+p.make_pizza(16,'aaaa')
